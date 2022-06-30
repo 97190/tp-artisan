a43e45b8911b65002bb246cb8e8fcf2ec5418e5b
melissa98m <melissa.mangione@gmail.com>
2022-06-30 12:34:26 +0200

    StatusController done

---
+++ app/Http/Controllers/StatusController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Status;
+use Illuminate\Http\Request;
+
+class StatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $statuses = Status::all();
+        return view('statuses.index', compact('statuses'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('statuses.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'status_name'=>'required',
+        ]);
+        Status::create([
+            'status_name' => $request->status_name
+        ]);
+
+        return redirect()->route('statuses.index')
+            ->with('success', 'Status créée avec succès !');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $status = Status::find($id);
+        return view('statuses.show', compact('status'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $status = Status::findOrFail($id);
+        return view('statuses.edit', compact('status'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $updateStatus = $request->validate([
+            'status_name' => 'required',
+        ]);
+        Status::whereId($id)->update($updateStatus);
+        return redirect()->route('statuses.index')
+            ->with('success', 'Le status a été mis à jour avec succes');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $status = Status::findOrFail($id);
+        $status->delete();
+        return redirect('/statuses')->with('success', 'Status supprimé');
+    }
+}
