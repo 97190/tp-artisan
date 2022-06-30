2befbeebac167071dc8543e8ee599d3a4158d2be
97190 <e.mayoute44@gmail.com>
2022-06-30 16:59:23 +0200

    Commentaires controller opération

--- app/Http/Controllers/OperationController.php
+++ app/Http/Controllers/OperationController.php
@@ -16,8 +16,8 @@ class OperationController extends Controller
      */
     public function index()
     {
-        $operations = Operation::all();
-        return view('operations.index', compact('operations'));
+        $operations = Operation::all(); // Toutes les opérations sont récuperées dans la variable $operations
+        return view('operations.index', compact('operations')); // On retourne la vue avec le listing des opérations avec le template index.blade.php et compact sert à afficher les opérations dans la vue avec les variables blade
     }
 
     /**
@@ -37,9 +37,9 @@ class OperationController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request) // La fonction store permets d'envoyer une requête pour pouvoir créer une opération dans la db
     {
-        $request->validate([
+        $request->validate([ // Recupération des valeurs saisies et validation de ces dernières dans le formulaire
             'nature_operation'=>'required',
             'date_operation'=>'required',
             'debit'=>'required',
@@ -49,7 +49,7 @@ class OperationController extends Controller
         
         ]);
 
-        $operation = Operation::create([
+        $operation = Operation::create([ // Assigne les valeurs saisies dans le formulaire au champs correspondant dans la bd (création de la nouvelle opération)
             'nature_operation' => $request->nature_operation,
             'date_operation' => $request->date_operation,
             'debit' => $request->debit,
@@ -61,7 +61,7 @@ class OperationController extends Controller
 
 
         return redirect()->route('operations.index')
-            ->with('success', 'Opération créer avec succès !');
+            ->with('success', 'Opération créer avec succès !'); // Une redirection vers la route index avec un succès 
     }
 
     /**
@@ -72,9 +72,9 @@ class OperationController extends Controller
      */
     public function show($id)
     {
-        $operation= Operation::find($id);
-        $category = $operation->category->category_name;
-        $status = $operation->status->status_name;
+        $operation= Operation::find($id); // Récupère une opération par rapport à son ID
+        $category = $operation->category->category_name; // On récupère le nom de la  categorie associé à l'opération
+        $status = $operation->status->status_name; // On récupère le nom du statut associé à l'opération
         return view('operations.show', compact('operation' , 'category' , 'status'));
     }
 
